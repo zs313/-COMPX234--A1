f15fb926b94895c3756820a5550d048a35933731
zhuoming shi <shizhuomin06@qq.com>
2026-03-23 20:27:46 +0800

    # Start all the threads
    run

--- Assignment1_help.py
+++ Assignment1_help.py
@@ -36,6 +36,10 @@ class Assignment1:
             self.pThreads.append(t)
         # Start all the threads
         # Write code here
+        for t in  self.mThreads:
+            t.start()
+        for t in  self.pThreads:
+            t.start()
 
         # Let the simulation run for some time
         time.sleep(self.SIMULATION_TIME)
