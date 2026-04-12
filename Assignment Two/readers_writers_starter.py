0d520b694253ad243cc1c7c5c66c965a2075463a
zhuoming shi <shizhuomin06@qq.com>
2026-04-12 14:20:46 +0800

    creare
    create 3 reader threads
    create 2 writer threads

--- Assignment Two/readers_writers_starter.py
+++ Assignment Two/readers_writers_starter.py
@@ -181,11 +181,13 @@ def main() -> None:
     #TODO: Create at least 3 Reader threads.
     readers = [
         Reader(reader_id=1, monitor=monitor)
+        Reader(reader_id=1, monitor=monitor)for i in range(1,4)#id 1 2 3
     ]
     
     #TODO: Create at least 2 writer threads.
     writers = [
         Writer(writer_id=1, monitor=monitor)
+        Writer(writer_id=1, monitor=monitor)for i in range(1,3)#id 1 2
     ]
 
     all_threads = readers + writers
